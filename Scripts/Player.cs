0a1c1e5009c598313f14a60b11f500af8a964bb0
Kacper Sobek <kacper.sobek2@gmail.com>
2018-11-01 12:50:28 +0100

    Init

---
+++ Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+	public Text CoinsText;
+	public int Coins;
+
+	private void Start() {
+		DrawText();
+	}
+
+	private void DrawText() {
+		CoinsText.text = "Monety: " + Coins;
+	}
+
+	private void OnCollisionEnter(Collision other) {
+		if (other.transform.name == "Coin") {
+			Coins++;
+			DrawText();
+			Destroy(other.gameObject);
+		}
+	}
+}
---
+++ Scripts/Spawner.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Spawner : MonoBehaviour {
+    public GameObject Prefab;
+    public int        ToSpawn;
+
+    private void Start() {
+        SpawnN(ToSpawn);
+    }
+
+    private GameObject Spawn() {
+        var obj = Instantiate(Prefab, GetSpawner().position, Quaternion.identity);
+        obj.transform.name = "Coin";
+        return obj;
+    }
+
+    private GameObject[] SpawnN(int n) {
+        List<GameObject> spawned = new List<GameObject>();
+        for (int i = 0; i < n; i++) {
+            spawned.Add(Spawn());
+        }
+        return spawned.ToArray();
+    }
+
+    private Transform GetSpawner() {
+        return transform.GetChild(Random.Range(0,transform.childCount));
+    }
+    
+}
